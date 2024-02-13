c49184230087daf311397ab0ceb896e860f05d46
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-13 23:50:10 +0600

    added a function that show and hide home and playground section

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 function play(){
-    console.log('play now');
+    // hide the home screen. to hide the screen add the class hidden to the home section
+    let homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+
+    // show the playground
+    let playGround = document.getElementById('play-ground');
+    playGround.classList.remove('hidden');
 }
