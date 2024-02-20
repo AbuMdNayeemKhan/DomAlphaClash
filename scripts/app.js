75a343e8ee54550bf0e2a4e816b018cdf669fda5
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-20 22:09:14 +0600

    added data

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// let audio = '';
+let audio = new Audio();
 function play(){
     // hide the home screen. to hide the screen add the class hidden to the home section
     let homeScreen = document.getElementById('home-screen');
@@ -82,7 +84,7 @@ function handleKeyboardKeyUpEvent(e){
         const currentLife = parseInt(currentLifeText);
 
         //add sound 
-        audio.src = '../audio/wrang.wav';
+        audio.src = "../audio/wrang.mp3";
         audio.play();
 
         //2. reduce the life count
